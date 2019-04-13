4810862249aecda01dee93b31e6dbdc3464d2000
Kevin Alfrich Woods Junior <samsausagehead@gmail.com>
2019-04-13 18:21:24 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,3 @@
- # Define a local variable called 'greeting',
+filter_parameter_logging # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+greating = Hello World
